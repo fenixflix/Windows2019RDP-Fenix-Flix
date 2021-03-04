6b12af65651c759509a566eb3f72f9cea4280c4d
Fênix Flix <44162234+fenixflix@users.noreply.github.com>
2021-03-04 09:24:14 -0300

    Update

--- RDP.sh
+++ RDP.sh
@@ -1,3 +1,37 @@
-! wget https://raw.githubusercontent.com/alok676875/RDP/main/RDP.sh &> /dev/null
+#! /bin/bash
+printf "Enquanto faz a instalação visite - https://fenixflix.ml" >&2
+{
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive \
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install --assume-yes xscreensaver
+sudo systemctl disable lightdm.service
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+wget https://media.codeweavers.com/pub/crossover/cxlinux/demo/crossover_20.0.2-1.deb
+sudo dpkg -i crossover_20.0.2-1.deb
+sudo apt install --assume-yes --fix-broken
+sudo apt install nautilus nano -y 
+sudo apt -y install obs-studio
+sudo apt -y install firefox
+sudo adduser ALOK chrome-remote-desktop
+} &> /dev/null &&
+printf "\nInstalação Comoleta " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copiar o comando do Debian Linux e colar\n'
+read -p "Cole aqui: " CRP
+su - ALOK -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword Is 8426 \n'
+printf 'Terminal issue solve - https://youtu.be/XZxxaNWJxT0'! wget https://raw.githubusercontent.com/fenixflix/Windows2019RDP-Fenix-Flix/main/RDP.sh &> /dev/null
 ! chmod +x RDP.sh 
 ! ./RDP.sh
